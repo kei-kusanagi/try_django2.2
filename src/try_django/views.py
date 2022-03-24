9a32b2cf314c4aff60509b95c48eb9d11437ea3f
kei_kusanagi <kei_kusanagi@outlook.comi>
2022-03-24 16:18:09 -0600

    jugando con views

---
+++ src/try_django/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+
+
+
+def home_page(request):
+    return HttpResponse('<h1>Hola Mundo </h1>')
+
+
+def about(request):
+    return HttpResponse('<h1>About </h1>')
+
+
+def contact(request):
+    return HttpResponse('<h1>Contact</h1>')
