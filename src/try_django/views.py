b503b066245946437733bdf9f3b49adfc9c20cd7
kei_kusanagi <kei_kusanagi@outlook.comi>
2022-03-25 16:15:19 -0600

    genial si funciono ponerle una imagen

--- src/try_django/views.py
+++ src/try_django/views.py
@@ -3,7 +3,11 @@ from django.shortcuts import render
 
 
 def home_page(request):
-    return render(request, 'hello_world.html')
+    my_title = "Rayquaza"
+    imagen = "https://pbs.twimg.com/media/EA1aIS9WsAA4QoR?format=jpg&name=large"
+    # doc = "<h1>{title}</h1>".format(title=title)
+    # django_render_doc = "<h1>{{title}}</h1>".format(title=title)
+    return render(request, 'hello_world.html', {"title": my_title, "imagen": imagen})
 
 
 def about(request):
