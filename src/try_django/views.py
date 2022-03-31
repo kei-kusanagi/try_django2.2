fb57b57446a36b21afa0a960f7e502ca08a55e06
kei_kusanagi <kei_kusanagi@outlook.comi>
2022-03-31 15:50:53 -0600

    pasar variables por la variable context

--- src/try_django/views.py
+++ src/try_django/views.py
@@ -6,7 +6,7 @@ from django.template.loader import get_template
 
 def home_page(request):
     my_title = "Rayquaza"
-    context = {"title": my_title}
+    context = {"title": my_title, "my_list": [1,2,3,4,5]}
     imagen = "https://pbs.twimg.com/media/EA1aIS9WsAA4QoR?format=jpg&name=large"
     # doc = "<h1>{title}</h1>".format(title=title)
     # django_render_doc = "<h1>{{title}}</h1>".format(title=title)
