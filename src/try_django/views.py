79bd3fd5a5cff8c2b94cbf5e7d2866bb8a9db1ea
kei_kusanagi <kei_kusanagi@outlook.comi>
2022-03-31 16:00:24 -0600

    usando el if en la funcion en ves de el html

--- src/try_django/views.py
+++ src/try_django/views.py
@@ -6,10 +6,11 @@ from django.template.loader import get_template
 
 def home_page(request):
     my_title = "Rayquaza"
-    context = {"title": my_title, "my_list": [1,2,3,4,5]}
     imagen = "https://pbs.twimg.com/media/EA1aIS9WsAA4QoR?format=jpg&name=large"
-    # doc = "<h1>{title}</h1>".format(title=title)
-    # django_render_doc = "<h1>{{title}}</h1>".format(title=title)
+    context = {"title": my_title}
+    if request.user.is_authenticated:
+        context = {"title": my_title, "my_list": [1,2,3,4,5], "imagen": imagen}
+    
     return render(request, 'home.html', context)
 
 
