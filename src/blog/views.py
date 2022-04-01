8a53d47303a8a28aa61d78c7db0fb03f3244b7a6
kei_kusanagi <kei_kusanagi@outlook.comi>
2022-04-01 15:27:34 -0600

    checando que nos dice el GET cuando lo mandamos a pedir a Django

--- src/blog/views.py
+++ src/blog/views.py
@@ -9,11 +9,12 @@ from .models import BlogPost
 # FILTER -> [] objects
 
 def blog_post_detail_page(request, slug):
+    print("DJANGO DICE", request.method, request.path, request.user)
     queryset = BlogPost.objects.filter(slug=slug)
     if queryset.count() == 0:
         raise Http404
     obj = queryset.first()
-    # obj = get_object_or_404(BlogPost, slug=slug)
+    # # obj = get_object_or_404(BlogPost, slug=slug)
     template_name = 'blog_post_detail.html'
     context = {"object": obj} # {"title": obj.title}
     return render(request, template_name, context)
