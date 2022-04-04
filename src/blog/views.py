b6377c283e20bf66cb2728ca25d87ff5bbcfea45
kei_kusanagi <kei_kusanagi@outlook.comi>
2022-04-04 15:59:41 -0500

    se borra el vista detallada ya que ya no se necesita

--- src/blog/views.py
+++ src/blog/views.py
@@ -5,19 +5,7 @@ from django.http import Http404
 # Create your views here.
 from .models import BlogPost
 
-# GET -> 1 object
-# FILTER -> [] objects
 
-def blog_post_detail_page(request, slug):
-    print("DJANGO DICE", request.method, request.path, request.user)
-    queryset = BlogPost.objects.filter(slug=slug)
-    if queryset.count() == 0:
-        raise Http404
-    obj = queryset.first()
-    # # obj = get_object_or_404(BlogPost, slug=slug)
-    template_name = 'blog_post_detail.html'
-    context = {"object": obj} # {"title": obj.title}
-    return render(request, template_name, context)
 
 
 def blog_post_list_view(request):
