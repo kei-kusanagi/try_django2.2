aac7acf87b8713e3dd9401d0b0241643ae95da5e
kei_kusanagi <kei_kusanagi@outlook.comi>
2022-04-04 17:58:08 -0500

    quedo mas limpio el directorio

--- src/blog/views.py
+++ src/blog/views.py
@@ -11,7 +11,7 @@ def blog_post_list_view(request):
     # puede ser una busqueda
     qs = BlogPost.objects.all() # SE MUESTRAN TODOS LOS BLOGS
     # qs = BlogPost.objects.filter(title__icontains='hola') ## SE MUESTRAN SOLO LOS QUE DIGAN 'hola'
-    template_name = 'blog_post_list.html'
+    template_name = 'blog/list.html'
     context = {'object_list': qs }
     return render(request, template_name, context)
 
@@ -19,7 +19,7 @@ def blog_post_list_view(request):
 def blog_post_create_view(request):
     # crear objetos
     # ? usar form
-    template_name = 'blog_post_create.html'
+    template_name = 'blog/create.html'
     context = {'form': None}
     return render(request, template_name, context)
 
@@ -27,20 +27,20 @@ def blog_post_create_view(request):
 def blog_post_detail_view(request, slug):
     # 1 objeto -> vista detallada
     obj = get_object_or_404(BlogPost, slug=slug)
-    template_name = 'blog_post_detail.html'
+    template_name = 'blog/detail.html'
     context = {"object": obj}
     return render(request, template_name, context)
 
 
 def blog_post_update_view(request, slug):
     obj = get_object_or_404(BlogPost, slug=slug)
-    template_name = 'blog_post_update.html'
+    template_name = 'blog/update.html'
     context = {"object": obj, 'form': None}
     return render(request, template_name, context)
 
 
 def blog_post_delete_view(request, slug):
     obj = get_object_or_404(BlogPost, slug=slug)
-    template_name = 'blog_post_delete.html'
+    template_name = 'blog/delete.html'
     context = {"object": obj, 'form': None}
     return render(request, template_name, context)
